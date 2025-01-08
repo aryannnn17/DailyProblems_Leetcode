2055653acd068bc1e92b7829af4747de9dfa3b94
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-01-07 19:56:24 -0800

    3042. Count Prefix and Suffix Pairs I

---
+++ 3042.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    bool isPrefixAndSuffix(string s1, string s2) {
+        if (s1.length() > s2.length())
+            return false;
+        if (s1 == s2.substr(0, s1.length()) &&
+            s1 == s2.substr(s2.length() - s1.length(), s1.length()))
+            return true;
+        return false;
+    }
+
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        if (words.size() == 1)
+            return 0;
+
+        int count = 0;
+        for (int i = 0; i < words.size() - 1; ++i) {
+            for (int j = i + 1; j < words.size(); ++j) {
+                count += isPrefixAndSuffix(words[i], words[j]);
+            }
+        }
+        return count;
+    }
+};
