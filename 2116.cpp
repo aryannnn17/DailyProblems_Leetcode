6afb470f9fbb0e956f0241d9473dbced134726ea
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-01-12 12:23:21 -0800

    01/12/2024 - 2116. Check if a Parentheses String Can Be Valid

---
+++ 2116.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+
+        if (s.length() % 2)
+            return false;
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || locked[i] == '0')
+                count++;
+            else
+                count--;
+            if (count < 0)
+                return false;
+        }
+        count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == ')' || locked[i] == '0')
+                count++;
+            else
+                count--;
+            if (count < 0)
+                return false;
+        }
+        return true;
+    }
+};
