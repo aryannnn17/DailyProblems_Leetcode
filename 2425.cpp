fd755be87ef8885c1f986ca5731c9524d929b06e
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-01-15 19:48:08 -0800

    01/16/2024 - 2425. Bitwise XOR of All Pairings

---
+++ 2425.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 0;
+        
+        if (nums1.size() % 2 == 1) {
+            for (auto& i : nums2)
+                ans ^= i;
+        }
+        if (nums2.size() % 2 == 1) {
+            for (auto& i : nums1)
+                ans ^= i;
+        }
+        return ans;
+    }
+};
