ad025dc91b2368053bdba082a048ce3691026756
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-01-16 16:56:25 -0800

    01/17/2024 - 2683. Neighboring Bitwise XOR

---
+++ 2683.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int ans = 0;
+        for (int& i : derived)
+            ans ^= i;
+        return (ans == 0) ? 1 : 0;
+    }
+};
