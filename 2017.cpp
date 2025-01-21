97ef23dad03124fdb273b7ae45ff42c658f5ad58
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-01-21 14:47:46 -0800

    01/2017. Grid Game

---
+++ 2017.cpp
@@ -0,0 +1,15 @@
+class Solution {
+#define ll long long
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        ll row1 = accumulate(begin(grid[0]), end(grid[0]), 0LL), row2 = 0;
+        ll R2 = LONG_LONG_MAX;
+        for (int i = 0; i < grid[0].size(); i++) {
+            row1 -= grid[0][i];
+            ll path = max(row1, row2);
+            R2 = min(R2, path);
+            row2 += grid[1][i];
+        }
+        return R2;
+    }
+};
