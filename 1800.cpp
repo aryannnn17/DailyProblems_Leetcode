ef4095b41ab3385419d5d809f11c3d42d7326f59
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-02-04 09:59:55 -0800

    02/04/2025 - 1800. Maximum Asc

---
+++ 1800.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int maxSum = nums[0];
+        int sum = nums[0];
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] < nums[i + 1])
+                sum += nums[i + 1];
+            else
+                sum = nums[i + 1];
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
+    }
+};
