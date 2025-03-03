0878199e56578ee1a12f71c13065bb7d786154db
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-03-02 18:03:35 -0800

    Create 2161.cpp

---
+++ 2161.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> ans, p2, p1;
+        for (int i : nums) {
+            if (i < pivot)
+                ans.push_back(i);
+            else if (i > pivot)
+                p2.push_back(i);
+            else
+                p1.push_back(i);
+        }
+        for (int i : p1)
+            ans.push_back(i);
+        for (int i : p2)
+            ans.push_back(i);
+        return ans;
+    }
+};
