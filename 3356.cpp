09205d1e441765a45d6d998391e69991ea747d15
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-03-15 15:03:14 -0700

    03/13/2025 - 3356. Zero Array Transformation II

---
+++ 3356.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int n;
+    int Q;
+
+    bool checkWithDifferenceArrayTeq(vector<int>& nums,
+                                     vector<vector<int>>& queries, int k) {
+        vector<int> diff(n, 0);
+        for (int i = 0; i <= k; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int x = queries[i][2];
+            diff[l] += x;
+            if (r + 1 < n)
+                diff[r + 1] -= x;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += diff[i];
+            diff[i] = sum;
+            if (nums[i] - diff[i] > 0)
+                return false;
+        }
+        return true;
+    }
+
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        n = nums.size();
+        Q = queries.size();
+        auto lambda = [](int x) { return x == 0; };
+        if (all_of(begin(nums), end(nums), lambda))
+            return 0;
+
+        int l = 0;
+        int r = Q - 1;
+        int k = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (checkWithDifferenceArrayTeq(nums, queries, mid) == true) {
+                k = mid + 1;
+                r = mid - 1;
+            } else
+                l = mid + 1;
+        }
+        return k;
+    }
+};
+
+// ----------< TLE >-----------
+// class Solution {
+// public:
+//     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+//         long sum = 0;
+//         for (int k : nums)
+//             sum += k;
+//         if (sum == 0)
+//             return 0;
+//         for (int i = 0; i < queries.size(); ++i) {
+//             long sum = 0;
+//             for (int j = queries[i][0]; j <= queries[i][1]; ++j) {
+//                 if (nums[j] > 0) {
+//                     if (nums[j] <= queries[i][2])
+//                         nums[j] = 0;
+//                     else
+//                         nums[j] -= queries[i][2];
+//                 }
+//             }
+//             for (int k : nums)
+//                 sum += k;
+//             if (sum == 0)
+//                 return i + 1;
+//         }
+//         return -1;
+//     }
+// };
