b8cbce43225ce71bc20129c1e71a5b66f514b9d3
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-04-09 12:43:59 -0700

    04/08/2025 - 1863. Sum of All Subset XOR Totals

---
+++ 1863.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int total = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            int sum = nums[i];
+            for (int j = i + 1; j < nums.size(); ++j) {
+                sum = sum ^ nums[j];
+                total += sum;
+            }
+        }
+        return total;
+    }
+};
