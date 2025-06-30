7ce1ceae49bb9e08063a6007edd0dd1c52695c9e
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-06-29 21:10:19 -0700

    05/29/2025 - 1498. Number of Subsequences That Satisfy the Given Sum Condition

---
+++ 1498.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int M = 1e9 + 7;
+
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int l = 0, r = nums.size() - 1;
+        vector<int> power(nums.size());
+        power[0] = 1;
+        for (int i = 1; i < nums.size(); i++)
+            power[i] = (power[i - 1] * 2) % M;
+
+        int ans = 0;
+        while (l <= r) {
+            if (nums[l] + nums[r] <= target) {
+                int diff = r - l;
+                ans = (ans % M + power[diff]) % M;
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return ans;
+    }
+};
