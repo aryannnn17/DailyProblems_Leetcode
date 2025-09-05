28a084e2f97bd5d173c546cd9ed200b3e51a67ce
Aryan Bhagat <aryanbhagat5702@gmail.com>
2025-09-05 13:44:11 -0700

    09/05/2025 - 2749. Minimum Operations to Make the Integer Zero

---
+++ 2749.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        for (int i = 1; i <= 36; i++) {
+            long val = (long)num1 - (long)i * num2;
+            if (val < 0)
+                return -1;
+            if (__builtin_popcountll(val) <= i && i <= val)
+                return i;
+        }
+        return -1;
+    }
+};
